1d9b324db68866e304c5c4e66054e4ec8d48e505
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 21:56:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-if phrase == upcase return "NO, NOT SINCE 1938!"
-elsif phrase == downcase return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "I LOVE YOU GRANDMA!" return " I LOVE YOU TOO PUMPKIN!"
+if 'phrase'.upcase return "NO, NOT SINCE 1938!"
+elsif 'phrase'.downcase return "HUH?! SPEAK UP, SONNY!"
+elsif 'phrase' == "I LOVE YOU GRANDMA!" return " I LOVE YOU TOO PUMPKIN!"
 end
 end
 
