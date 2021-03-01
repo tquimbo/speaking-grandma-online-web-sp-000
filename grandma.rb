b736a0075454c2c575a17d6fe7235db68aed428b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 22:19:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.downcase
+elsif phrase != "I LOVE YOU GRANDMA!"
   puts "HUH?! SPEAK UP, SONNY!"
 
 end
