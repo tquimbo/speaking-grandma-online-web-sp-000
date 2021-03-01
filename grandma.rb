58ef4ec98e6df2f9aa48d76a1ca4c986dae9bd37
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 22:12:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 if "#{phrase}".downcase puts "NO, NOT SINCE 1938!"
-elsif "#{phrase}".uppercase puts "HUH?! SPEAK UP, SONNY!"
+elsif "#{phrase}".upcase puts "HUH?! SPEAK UP, SONNY!"
 elsif "#{phrase}" == "I LOVE YOU GRANDMA!" puts " I LOVE YOU TOO PUMPKIN!"
 binding.pry
 end
